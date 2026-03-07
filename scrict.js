174202d71bb846c6bc51435735fceb64c0f03d40
Arafathosen2001 <aarafat985@gmail.com>
2026-03-07 22:14:35 +0600

    add open and colse section dynamicly || 2026-03-07 10:14 PM

--- scrict.js
+++ scrict.js
@@ -1,9 +1,65 @@
+
+const sectionAll = document.getElementById('main-content');
+const sectionOpen = document.getElementById('section-open');
+const sectionClosed = document.getElementById('section-close');
+
+const totalContent = document.getElementById('total');
+const openContent = document.getElementById('open');
+const closeContent = document.getElementById('close');
+
+const allBtn = document.getElementById('all-btn');
+const openBtn = document.getElementById('open-btn');
+const closeBtn = document.getElementById('close-btn');
+
 const switchTab = (tab) => {
     // console.log(tab);
+    if (tab === 'all') {
+        // console.log('tab-all');
+        sectionAll.classList.remove('hidden')
+        sectionOpen.classList.add('hidden')
+        sectionClosed.classList.add('hidden')
+
+        openContent.classList.add('hidden')
+        totalContent.classList.remove('hidden')
+        closeContent.classList.add('hidden')
+
+
+        
+    }
+    if (tab === 'open') {
+        // console.log('tab-open');
+        sectionOpen.classList.remove('hidden')
+        sectionAll.classList.add('hidden')
+        sectionClosed.classList.add('hidden')
+
+
+        openContent.classList.remove('hidden')
+        totalContent.classList.add('hidden')
+        closeContent.classList.add('hidden')
+        
+        
+    }
+    if (tab === 'close') {
+        // console.log('tab-open');
+        sectionOpen.classList.add('hidden')
+        sectionAll.classList.add('hidden')
+        sectionClosed.classList.remove('hidden')
+
+        openContent.classList.add('hidden')
+        totalContent.classList.add('hidden')
+        closeContent.classList.remove('hidden')
+    }
 }
-const loadAllIissues = () => {
+const  loadAllIissues = async() => {
     const url = "https://phi-lab-server.vercel.app/api/v1/lab/issues";
-    fetch(url).then((res) => res.json()).then((data) => displyIssues(data.data))
+    const res = await fetch(url);
+    const data = await res.json();
+    //     .then((res) => res.json()).then((data) => {
+        displyIssues(data.data);
+        closeOpenIssue(data.data);
+
+    // })
+    // console.log(data.data);
 };
 loadAllIissues();
 
@@ -14,10 +70,12 @@ const creatLabels = (labels)=>{
 const allIssusContainer = document.getElementById('main-content');
 const displyIssues = (data) => {
     // console.log(issu.labels);
- 
+
     
-    // console.log(data);
     data.forEach(issu => {
+        // const FindInterview = issu.status.find(job => job.open === "open");
+        // console.log(FindInterview);
+  
         
         const card = document.createElement('div');
         card.innerHTML = `
@@ -58,9 +116,15 @@ const displyIssues = (data) => {
 };
 const countIssues = () => {
     const totalCount = allIssusContainer.children.length;
-    // console.log(totalCount);
-    const totalContent = document.getElementById('total');
+    const openCount = sectionOpen.children.length;
+    const closeCount = sectionClosed.children.length;
+    
     totalContent.innerText = totalCount;
+    
+    openContent.innerText = openCount;
+    
+    closeContent.innerText = closeCount;
+    // console.log(closeCount);
 }
 
 const loadSingelIssues = (id) => {
@@ -106,5 +170,91 @@ const displySingelIssues = (issu) => {
                     modalBox.append(modalDiv);
                     const modalId = document.getElementById('issues_modal').showModal();
 }
+
+const closeOpenIssue = (data)=>{
+    const openIssues = data.filter(issue => issue.status === "open");
+    const closedIssues = data.filter(issue => issue.status === "closed");
+
+    
+    // console.log(openIssues);
+    // console.log(closedIssues);
+    openIssues.forEach(issu => {
+        // console.log(issu)
+
+
+        const card = document.createElement('div');
+        card.innerHTML = `
+            <div id="card" class="cadr cursor-pointer h-full ${issu.status == "open" ? "border-t-3 border-green-500" : "border-t-3 border-purple-500"} p-3 bg-white rounded-md space-y-3 " onclick="loadSingelIssues(${issu.id})">
+            <div class="flex justify-between items-center">
+                <div>
+                    ${issu.status == "open" ? `<img class="w-6 h-6" src="./assets/Open-Status.png" alt="">` : `<img class="w-6 h-6" src="./assets/Closed- Status .png" alt="">`}
+                </div>
+                <button class="btn btn-soft btn-secondary rounded-3xl">${issu.priority}</button>
+            </div>
+            <div class="con">
+                <h3 class="font-semibold line-clamp-1">${issu.title}</h3>
+                <p class="text-gray-500 line-clamp-2">${issu.description}</p>
+            </div>
+            <div class="badg flex gap-2">
+            
+                <div class="">${creatLabels(issu.labels)}</div>
+            </div>
+            <hr class="text-gray-500 border border-gray-500">
+            <div class="date flex justify-between">
+                <div>
+                    <p class="text-gray-500">#${issu.author}</p>
+                    <p class="text-gray-500">${issu.assignee}</p>
+                </div>
+                <div>
+                    <p class="text-gray-500 text-right">${issu.createdAt}</p>
+                    <p class="text-gray-500">${issu.updatedAt}</p>
+                </div>
+            </div>
+
+        </div>
+        `
+        sectionOpen.append(card);
+        countIssues();
+    })
+    closedIssues.forEach(issu => {
+        // console.log(issu)
+
+
+        const card = document.createElement('div');
+        card.innerHTML = `
+            <div id="card" class="cadr cursor-pointer h-full ${issu.status == "open" ? "border-t-3 border-green-500" : "border-t-3 border-purple-500"} p-3 bg-white rounded-md space-y-3 " onclick="loadSingelIssues(${issu.id})">
+            <div class="flex justify-between items-center">
+                <div>
+                    ${issu.status == "open" ? `<img class="w-6 h-6" src="./assets/Open-Status.png" alt="">` : `<img class="w-6 h-6" src="./assets/Closed- Status .png" alt="">`}
+                </div>
+                <button class="btn btn-soft btn-secondary rounded-3xl">${issu.priority}</button>
+            </div>
+            <div class="con">
+                <h3 class="font-semibold line-clamp-1">${issu.title}</h3>
+                <p class="text-gray-500 line-clamp-2">${issu.description}</p>
+            </div>
+            <div class="badg flex gap-2">
+            
+                <div class="">${creatLabels(issu.labels)}</div>
+            </div>
+            <hr class="text-gray-500 border border-gray-500">
+            <div class="date flex justify-between">
+                <div>
+                    <p class="text-gray-500">#${issu.author}</p>
+                    <p class="text-gray-500">${issu.assignee}</p>
+                </div>
+                <div>
+                    <p class="text-gray-500 text-right">${issu.createdAt}</p>
+                    <p class="text-gray-500">${issu.updatedAt}</p>
+                </div>
+            </div>
+
+        </div>
+        `
+        sectionClosed.append(card);
+        countIssues();
+    })
+}
+
 
 
